bdb92479f6e6fc5a81894cfd0541b32585a66005
Matt Willy <matt.willy@gmail.com>
2020-05-07 13:31:46 -0600

    add the ability to select temperature units (#12)

--- lib/weather_application_web/live/page_live.ex
+++ lib/weather_application_web/live/page_live.ex
@@ -4,36 +4,54 @@ defmodule WeatherApplicationWeb.PageLive do
   """
   use WeatherApplicationWeb, :live_view
 
+  alias WeatherApplication.RequestParams
+
+  @base_url "http://api.openweathermap.org/data/2.5/weather?"
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, zip: "", temperature: "", results: %{})}
+    {:ok, assign(socket, zip: "", temperature: "", units: "imperial", results: %{})}
   end
 
   @impl true
-  @spec handle_event(<<_::48>>, map, Phoenix.LiveView.Socket.t()) ::
-          {:noreply, Phoenix.LiveView.Socket.t()}
-  def handle_event("search", %{"zip" => zip}, socket) do
-    temperature = parse_weather(zip)
+  def handle_event("search", %{"zip" => zip}, %{assigns: %{units: units}} = socket) do
+    temperature = parse_weather(%RequestParams{zip: zip, units: units})
 
     {:noreply, assign(socket, zip: zip, temperature: temperature)}
   end
 
-  @spec parse_weather(String.t()) :: String.t()
-  defp parse_weather(zip) do
-    api_key = Application.get_env(:weather_application, :openweather_api_key)
-
-    url =
-      String.to_charlist(
-        "http://api.openweathermap.org/data/2.5/weather?zip=#{zip}&units=imperial&appid=#{api_key}"
-      )
-
-    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {url, []}, [], [])
-
-    {:ok, %{"main" => %{"temp" => temp}}} =
-      body
-      |> List.to_string()
-      |> Jason.decode()
-
-    temp
+  @impl true
+  def handle_event("unit-selected", %{"value" => units}, socket) do
+    {:noreply, assign(socket, :units, units)}
   end
+
+  @spec parse_weather(map()) :: String.t()
+  defp parse_weather(%RequestParams{zip: zip} = params) do
+    case zip do
+      "" ->
+        ""
+
+      _zip ->
+        url = create_url(params)
+        {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {url, []}, [], [])
+
+        {:ok, %{"main" => %{"temp" => temp}}} =
+          body
+          |> List.to_string()
+          |> Jason.decode()
+
+        temp
+    end
+  end
+
+  @spec create_url(RequestParams.t()) :: charlist()
+  defp create_url(%RequestParams{zip: zip, units: units}) do
+    String.to_charlist("#{base_url()}zip=#{zip}&units=#{units}&appid=#{api_key()}")
+  end
+
+  @spec base_url() :: String.t()
+  defp base_url, do: @base_url
+
+  @spec api_key() :: String.t()
+  defp api_key, do: Application.get_env(:weather_application, :openweather_api_key)
 end
