55d30909428b2f8a67ef7aa69633bf56de793a13
Matt Willy <matt.willy@gmail.com>
2020-05-07 14:14:14 -0600

    setup for a release

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :weather_application, WeatherApplicationWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
