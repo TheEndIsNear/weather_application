e4bc1b6d02c8007aede0290ac11b7dc2cef05b94
Matt Willy <matt.willy@gmail.com>
2020-05-08 09:27:49 -0600

    validate that the zip code is a proper zip code, before attempting to run the api call

--- lib/weather_application/request_params.ex
+++ lib/weather_application/request_params.ex
@@ -2,8 +2,26 @@ defmodule WeatherApplication.RequestParams do
   @moduledoc """
   Module for handling all of the request params being passed to open weather map
   """
-  defstruct zip: "", units: "imperial"
+  use Ecto.Schema
+  import Ecto.Changeset
 
   @type t(zip, units) :: %__MODULE__{zip: zip, units: units}
   @type t :: %__MODULE__{zip: String.t(), units: String.t()}
+
+  schema "request_params" do
+    field :zip, :string
+    field :units, :string
+  end
+
+  def changeset(params \\ %{}) do
+    %__MODULE__{}
+    |> changeset(params)
+  end
+
+  def changeset(request_params, attrs) do
+    request_params
+    |> cast(attrs, [:zip, :units])
+    |> validate_required([:zip])
+    |> validate_format(:zip, ~r/^[0-9]{5}(?:-[0-9]{4})?$/)
+  end
 end
